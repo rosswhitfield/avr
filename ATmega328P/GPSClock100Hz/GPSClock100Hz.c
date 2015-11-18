7ee825f2ba1802a7bbeda75f750cc2a62dc4fe9b
Ross Whitfield <ross.whitfield@gmail.com>
2015-11-18 09:53:45 -0500

    Update pin info

--- ATmega328P/GPSClock100Hz/GPSClock100Hz.c
+++ ATmega328P/GPSClock100Hz/GPSClock100Hz.c
@@ -16,9 +16,9 @@ uint8_t digit[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 void writeDigit(uint8_t);
 
 int main() {
-  // PB0 - Data (DS)
-  // PB1 - Shift clock (SH_CP)
-  // PB2 - Store Clock (ST_CP)
+  // PB0 - Data
+  // PB1 - Clock
+  // PB2 - Latch
   DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2);
   while (1) {
     for (uint8_t i = 0; i < 10; i++) {
